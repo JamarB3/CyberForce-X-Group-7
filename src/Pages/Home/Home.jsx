4e7b792f09f851deb6c9dfca0b59a4336a841155
Christian B <batsonfocus@gmail.com>
2025-01-28 18:43:43 -0500

    Navbar + Homepage skeleton + LoginModal initialization

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Home.css'
+import Coffee from '../../assets/Coffee-png.png'
+import Full from '../../assets/Stars/Full.svg'
+import Half from '../../assets/Stars/Half.svg'
+import Empty from '../../assets/Stars/Empty.svg'
+import BusinessCard from '../../Components/BusinessCard/BusinessCard'
+
+const Home = () => {
+  return (
+    <div>
+
+      {/* Landing/Welcome to InclusiFind */}
+
+      <div className="flexContainer blueBox">
+        <div className="flexContainer">
+          <h1>Welcome to InclusiFind</h1>
+        </div>
+      </div>
+
+
+      {/* Businesses Near You? Skeleton */}
+
+      <div className=''>
+        <p className="topLeft">Businesses Near You</p>
+        <div className='flexContainer'>
+          <div className="gridContainer">
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+
+
+
+          </div>
+        </div>
+      </div>
+        
+      {/* Footer possible? */}
+        
+
+    </div>
+  )
+}
+
+export default Home
