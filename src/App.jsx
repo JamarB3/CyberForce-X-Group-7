5912fad91b7b8455b7b32e258f99266f72504d71
Kaleb Berry <kaleb.berry@bison.howard.edu>
2025-02-01 12:58:34 -0500

    Login Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <div>
-      Our App!
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Home.css'
+import Coffee from '../../assets/Coffee-png.png'
+import Full from '../../assets/Stars/Full.svg'
+import Half from '../../assets/Stars/Half.svg'
+import Empty from '../../assets/Stars/Empty.svg'
+import BusinessCard from '../../components/BusinessCard/BusinessCard.jsx'
+
+const Home = () => {
+  console.log("Home component rendered");
+  return (
+    <div>
+
+      {/* Landing/Welcome to InclusiFind */}
+
+      <div className="flexContainer blueBox">
+        <div className="flexContainer">
+          <h1>Welcome to InclusiFind</h1>
+        </div>
+      </div>
+
+
+      {/* Businesses Near You? Skeleton */}
+
+      <div className=''>
+        <p className="topLeft">Businesses Near You</p>
+        <div className='flexContainer'>
+          <div className="gridContainer">
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+            <BusinessCard/>
+
+
+
+          </div>
+        </div>
+      </div>
+        
+      {/* Footer possible? */}
+        
+
+    </div>
+  );
+};
+
+export default Home;
