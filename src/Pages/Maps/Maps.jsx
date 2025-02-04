06a86c6522e1d1c936c06209f3dcfcccc33b72cf
Xero-Xis <miles.adedjouma@bison.howard.edu>
2025-02-04 11:06:13 -0500

    Set up titles for potential categories

---
+++ src/Pages/Maps/Maps.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Maps.css'
+
+
+
+const Maps= () => {
+    return (
+      <div>
+<div className="flexContainer blueBox">
+        <div className="flexContainer">
+          <h1> Maps</h1>
+
+        </div>
+      </div>
+
+
+
+</div>
+  )
+}
