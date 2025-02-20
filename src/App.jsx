d7dcc78c9b560f6ddc4a7c99df7af07ef3539de6
Kaleb Berry <kaleb.berry@bison.howard.edu>
2025-02-20 12:11:57 -0500

    profile creation pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import ProfileCreation from './pages/ProfileCreation/ProfileCreation';
+import BusinessProfileCreation from './pages/BusinessProfileCreation/BusinessProfileCreation';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/profile' element={<ProfileCreation/>}/>
+        <Route path='/businessprofile' element={<BusinessProfileCreation/>}/>
+        
       </Routes>
     </div>
   );
